a2f7b916f46e3939e96923177e79fb8dc3b67c7d
Victor Gargurevich <MeloVicG41@gmail.com>
2021-01-12 09:53:16 -0800

    understanding Django

---
+++ time_display_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from time import gmtime, strftime
+
+
+# Create your views here.
+def index(request):
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M:%S %p", gmtime())
+    }
+    return render(request, 'index.html', context)
+
